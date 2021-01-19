2d86373a474f8c327c067b49ca58d865eb136d5f
hwee1115 <hwee1115@naver.com>
2021-01-20 00:10:21 +0900

    01-20

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -3,13 +3,16 @@ import axios from 'axios';
 function LandingPage() {
     
     useEffect(()=>{
-        axios.get('/api/hello')
+        axios.get('/api/hello') //Request를 보내기 위한 라이브러리
         .then(response => console.log(response.data))
     },[])
 
     return (
-        <div>
-            LandingPage
+        <div style={{
+            display:'flex', justifyContent:'center', alignItems:'center'
+            ,width:'100%', height:'100vh'
+        }}>
+            <h2>시작페이지</h2>
         </div>
     )
 }
